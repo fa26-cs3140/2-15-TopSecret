fddcca6738269e059d6b9fe0ad59964e45f35a17
za-uv <krj3fe@virginia.edu>
2026-09-23 14:02:16 -0400

    gradle run setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,10 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main 
+public class TopSecret
 {
-    static void main() 
+    static void main(String[] args) 
     {
         
-        }
     }
 }
